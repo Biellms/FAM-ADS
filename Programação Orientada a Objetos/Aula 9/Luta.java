29d2e3d60e20f876117ed06573afb6e4cf2ba462
Biellms <biell.mendes8@gmail.com>
2022-04-15 00:11:27 -0300

    Add classe Luta / Att Main

--- Programação Orientada a Objetos/Aula 9/Luta.java
+++ Programação Orientada a Objetos/Aula 9/Luta.java
@@ -1,11 +1,99 @@
 
 package ufc;
 
+import java.util.*;
+
 public class Luta {
     
+    Random aleatorio = new Random();
+    Scanner ler = new Scanner(System.in);
+
+    // Atributos
     private Lutador desafiado;
     private Lutador desafiante;
     private int rounds;
     private Boolean status;
 
+    // Constructor
+    public Luta() {
+
+    }
+
+    // Métodos
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        if(l1.getCategoria() == l2.getCategoria()) {
+            status = true;
+            desafiado = l1;
+            desafiante = l2;
+        } else {
+            status = false;
+            desafiado = null;
+            desafiante = null;
+        }
+    }
+
+    public void lutar() {
+        if(status) {
+            
+            desafiado.status();
+            desafiante.status();
+            
+            int vencedor = aleatorio.nextInt(3);
+
+            switch (vencedor) {
+                case 0:
+                    System.out.println(" A luta empatou\n");
+                    desafiado.empatarLuta();
+                    desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println(" O "+desafiado.getNome()+" venceu a luta!\n");
+                    desafiado.ganharLuta();
+                    desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println(" O "+desafiante.getNome()+" venceu a luta!\n");
+                    desafiado.perderLuta();
+                    desafiante.ganharLuta();
+                    break;
+                }
+
+        } else {
+            System.out.println(" A categoria dos lutadores são diferentes\n "
+            + "Por isso a luta não pode acontecer!\n");
+        }
+    }
+
+    // Getters and Setters
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
--- Programação Orientada a Objetos/Aula 9/Main.java
+++ Programação Orientada a Objetos/Aula 9/Main.java
@@ -1,12 +1,11 @@
 
 package ufc;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        Luta ufc = new Luta();
         Lutador l[] = new Lutador[6];
 
         l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
@@ -16,21 +15,30 @@ public class Main {
         l[4] = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
         l[5] = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
 
+        System.out.println("\n\t######## UFC ######## \n");
+
+        System.out.println(" ----------------------------------------");
+        System.out.println(" LUTADORES");
+        System.out.println(" ----------------------------------------");
         l[0].apresentar();
         l[1].apresentar();
         l[2].apresentar();
         l[3].apresentar();
         l[4].apresentar();
         l[5].apresentar();
-
         System.out.println(" ----------------------------------------");
-
-        l[0].status();
-        l[1].status();
-        l[2].status();
-        l[3].status();
-        l[4].status();
-        l[5].status();
-
+        System.out.println(" LUTAS DE HOJE");
+        System.out.println(" ----------------------------------------");
+        ufc.marcarLuta(l[0], l[1]);
+        ufc.lutar();
+        System.out.println(" ----------------------------------------");
+        ufc.marcarLuta(l[2], l[3]);
+        ufc.lutar();
+        System.out.println(" ----------------------------------------");
+        ufc.marcarLuta(l[4], l[5]);
+        ufc.lutar();
+        System.out.println(" ----------------------------------------");
+        ufc.marcarLuta(l[3], l[5]);
+        ufc.lutar();
     }
 }
